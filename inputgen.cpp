dfe41255872a0e3b40b00af881023541a25c65f1
Shrikant Jha <shrikantjha11@gmail.com>
2019-07-01 13:15:41 +0530

    Add files via upload

---
+++ inputgen.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+int main()
+{
+	ll t;
+	t = 100;
+	cout<<100<<endl;
+	while(t--)
+	{
+		int a = rand()%100 ;
+		int b = rand()%100 ;
+		cout<<a<<" "<<b<<endl;
+	}
+}
+
